9e3e1a5059b880a576014c177d38828f319ef038
Mohammed Elkhiat <mohammed.khayyat02@eng-st.cu.edu.eg>
2025-06-08 19:00:02 +0300

    Added hf_token to .env

--- extract.py
+++ extract.py
@@ -1,11 +1,13 @@
 import argparse
-from src.feature_extraction.utils import login_hf
 from src.feature_extraction.df_utils import extract_and_save
+from dotenv import load_dotenv
+
 
 if __name__ == '__main__':
+    load_dotenv()
+
     parser = argparse.ArgumentParser(description='Extract features from dataset using a given model.')
     
-    parser.add_argument('--hf_token', type=str, required=True, help='Hugging Face token for authentication.')
     parser.add_argument('--model_name', type=str, required=True, help='Model name (e.g. PanDerm, clip, derm).')
     parser.add_argument('--data_name', type=str, required=True, help='Dataset name (e.g. ham, d7p, dmf).')
     # parser.add_argument('--start', type=int, default=0, help='Start index for image slicing.')
@@ -14,23 +16,16 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
-    print(f"Logging into Hugging Face with token.")
-    login_hf(token=args.hf_token)
 
     print(f"Extracting features from {args.data_name} using {args.model_name}")
     extract_and_save(
         model_name=args.model_name,
-        data_name=args.data_name,
-        start=0,
-        end=20,
-        batch_size=10
+        data_name=args.data_name
     )
 
 
-#TODO: Add hf_token to .env for more security 
-    # Example usage:
+# Example usage:
 #     python extract.py \
-#   --hf_token sk-xxx \
 #   --model_name panderm \
 #   --data_name dmf \
 
--- src/feature_extraction/df_utils.py
+++ src/feature_extraction/df_utils.py
@@ -32,7 +32,7 @@ def create_df_paths(model_name, data_name):
     suffix = data_name
     
     aux_path = os.path.join('outputs', f'{prefix}_{suffix}_pre.csv')
-    output_path = os.path.join('outputs', f'{prefix}_{suffix}_.csv')
+    output_path = os.path.join('outputs', f'{prefix}_{suffix}.csv')
 
     return aux_path, output_path
 
@@ -47,7 +47,7 @@ def extract_and_save(model_name, data_name, start=0, end = None, batch_size = 10
         rslt_dict = pths,
         start=start, end = end,
         batch_size = batch_size, 
-        model_name = model_name, ## Options: 'PanDerm', 'derm-foundation', 'vit'
+        model_name = model_name, ## Options: 'panderm', 'derm', 'clip'
         data_name = data_name, ## Options: 'ham', 'd7p', 'dmf'
         output_csv = aux_path)
     
